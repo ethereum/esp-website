6ccc6522aeb96eb381c9bbccd8f717d2b5045232
Sam Richards <sbrichards@gmail.com>
2019-11-30 18:08:49 -0800

    Fix case build issue

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,9 +6,9 @@
 
 // You can delete this file if you're not using it
 
-import React from "react";
-import Layout from "./src/components/Layout";
+import React from "react"
+import Layout from "./src/components/layout"
 
 export const wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>;
-};
+  return <Layout {...props}>{element}</Layout>
+}
