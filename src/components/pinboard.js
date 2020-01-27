beaf4ddb1a8d93d18577e9e6544fcb78c5e40e08
Sam Richards <sbrichards@gmail.com>
2020-01-27 12:57:37 -0800

    Add basic pinboard loading state

--- src/components/pinboard.js
+++ src/components/pinboard.js
@@ -1,24 +1,41 @@
 import React, { useState, useEffect } from "react"
 
 const Pinboard = () => {
-  const [pins, setPins] = useState(0) // TODO set initial state for loading pins
+  const [pinboardState, setPinboardState] = useState({
+    pins: [],
+    loading: true,
+  })
+
   useEffect(() => {
     fetch("/.netlify/functions/pinboard")
       .then(response => response.json())
       .then(result => {
-        setPins(result.data.records)
+        setPinboardState({ pins: result.data.records, loading: false })
+      })
+      .catch(e => {
+        console.error(e)
       })
   }, [])
 
   return (
     <>
       <h3>Pinboard</h3>
+      {pinboardState.loading && <h3>Loading...</h3>}
       <ul>
-        {pins.length &&
-          pins.map((item, i) => (
+        {pinboardState.pins.length &&
+          pinboardState.pins.map((item, i) => (
             <li key={item.id}>
-              <p>{item.fields.Title}</p>
-              <p>{item.fields.Description}</p>
+              <p>Post ID: {item.fields["Post ID"]}</p>
+              <p>Title: {item.fields["Title"]}</p>
+              <p>Short Description: {item.fields["Short Description"]}</p>
+              <p>Long Description: {item.fields["Long Description"]}</p>
+              <p>Submission Form: {item.fields["Submission Form"]}</p>
+              <p>
+                {/* TODO update to pathname to use internal links */}
+                <a href={item.fields["Submission Form"]}>Apply here</a>
+              </p>
+              <p>Posted By: {item.fields["Posted By"]}</p>
+              <p>Source URL: {item.fields["Source URL"]}</p>
             </li>
           ))}
       </ul>
