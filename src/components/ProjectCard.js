e894d863daa630020b3d3e55696977bf8f335489
Sam Richards <sbrichards@gmail.com>
2020-02-06 16:40:59 -0800

    Add ProjectCard mobile styles

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,9 +3,15 @@ import { Link } from "gatsby"
 import styled from "styled-components"
 
 import { StyledLink, CardImage } from "../components/SharedStyledComponents"
-import { colorGray, colorMedGray } from "../utils/styles"
+import {
+  colorGray,
+  colorMedGray,
+  screenSizeL,
+  screenSizeM,
+} from "../utils/styles"
 
 const Card = styled(Link)`
+  max-height: 600px;
   width: 100%;
   border-radius: 10px;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.16);
@@ -13,9 +19,12 @@ const Card = styled(Link)`
   color: black;
 
   margin-bottom: 24px;
-  padding: 8px;
   display: flex;
   justify-content: space-between;
+
+  @media (max-width: ${screenSizeM}) {
+    flex-direction: column;
+  }
 
   transition: 0.4s ease-out;
 
@@ -33,6 +42,29 @@ const CardBody = styled.div`
   flex: 1 1 300px;
 `
 
+const CardHeader = styled.header`
+  display: flex;
+  margin: 24px 0;
+
+  @media (max-width: ${screenSizeL}) {
+    flex-direction: column;
+  }
+  @media (max-width: ${screenSizeM}) {
+    text-align: center;
+  }
+`
+
+const CardTitle = styled.div`
+  font-size: 1.62671rem;
+  font-weight: 500;
+  margin-right: 16px;
+  white-space: nowrap;
+
+  @media (max-width: ${screenSizeL}) {
+    margin-bottom: 16px;
+  }
+`
+
 const CardGrant = styled.div`
   flex: 0 1 160px;
   display: flex;
@@ -40,12 +72,22 @@ const CardGrant = styled.div`
   justify-content: space-between;
 `
 
+const CardCategory = styled.div`
+  color: ${colorGray};
+  font-size: 0.9rem;
+  white-space: nowrap;
+`
+
 const LighterText = styled.div`
   color: ${colorGray};
+  font-size: 0.9rem;
 `
 
 const DetailsLink = styled(StyledLink)`
   font-weight: bold;
+  @media (max-width: ${screenSizeM}) {
+    text-align: center;
+  }
 `
 
 const ProjectCard = ({ frontmatter, link }) => {
@@ -56,10 +98,10 @@ const ProjectCard = ({ frontmatter, link }) => {
         alt="Ecosystem Support Program Process"
       />
       <CardBody>
-        <h2>{frontmatter.title}</h2>
-        <LighterText>
-          <p>{frontmatter.category}</p>
-        </LighterText>
+        <CardHeader>
+          <CardTitle>{frontmatter.title}</CardTitle>
+          <CardCategory>{frontmatter.category}</CardCategory>
+        </CardHeader>
         <div>{frontmatter.description}</div>
       </CardBody>
       <CardGrant>
