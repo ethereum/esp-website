23130afa34ac37b1bbb66ff5c5a269d60ade0d1e
Sam Richards <sbrichards@gmail.com>
2020-05-05 13:04:12 -0700

    Update footer styles & links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,53 +1,160 @@
 import React from "react"
+import styled from "styled-components"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faTwitter, faGithub } from "@fortawesome/free-brands-svg-icons"
+import {
+  colorGrayDarker,
+  colorRed,
+  screenSizeS,
+  screenSizeL,
+  screenSizeM,
+} from "../utils/styles"
+
+const FooterElement = styled.footer`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 30px;
+  color: ${colorGrayDarker};
+
+  @media (max-width: ${screenSizeS}) {
+    padding: 15px;
+  }
+`
+
+const LinkContainer = styled.div`
+  display: flex;
+  @media (max-width: ${screenSizeM}) {
+    flex-direction: column;
+    align-items: center;
+    margin-top: 8px;
+  }
+`
+const IconContainer = styled.div``
+
+const Link = styled.a`
+  color: ${colorGrayDarker};
+  margin-left: 16px;
+  margin-right: 16px;
+
+  &:hover {
+    color: ${colorRed};
+  }
+
+  @media (max-width: ${screenSizeL}) {
+    margin-left: 12px;
+    margin-right: 12px;
+  }
+`
+
+const Icon = styled(FontAwesomeIcon)`
+  color: ${colorGrayDarker};
+  font-size: 32px;
+  margin: 16px 8px;
+
+  &:hover {
+    color: ${colorRed};
+  }
+`
+
+const Legal = styled.div`
+  font-size: 0.8rem;
+  @media (max-width: ${screenSizeS}) {
+    text-align: center;
+  }
+`
+
+const Bar = styled.div`
+  @media (max-width: ${screenSizeM}) {
+    display: none;
+  }
+`
+
+const BR = styled.br`
+  display: none;
+  @media (max-width: ${screenSizeS}) {
+    display: block;
+  }
+`
+
+export const Divider = styled.div`
+  height: 3px;
+  background-image: linear-gradient(to right, #ffcf47, #c6566c);
+`
 
 const Footer = () => (
-  <footer>
-    <div className="copy footer-legal">
-      © {new Date().getFullYear()} Ethereum Foundation. All rights reserved.
-    </div>
-    <div className="soc">
-      <a
-        className="copy"
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.ethereum.org/"
-      >
-        Ethereum.org
-      </a>
-      <a
-        className="copy"
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.ethereum.org/privacy-policy/"
-      >
-        Privacy Policy
-      </a>
-      <a
-        className="copy"
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.ethereum.org/cookie-policy/"
-      >
-        Cookie Policy
-      </a>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://twitter.com/EF_ESP"
-      >
-        <FontAwesomeIcon icon={faTwitter} />
-      </a>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://github.com/ethereum"
-      >
-        <FontAwesomeIcon icon={faGithub} />
-      </a>
-    </div>
-  </footer>
+  <>
+    <Divider />
+    <FooterElement>
+      <LinkContainer>
+        <Link
+          className="copy"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.ethereum.org/"
+        >
+          Ethereum.org
+        </Link>
+        <Bar>|</Bar>
+        <Link
+          className="copy"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://ethereum.foundation/"
+        >
+          Ethereum.foundation
+        </Link>
+        <Bar>|</Bar>
+        <Link
+          className="copy"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://blog.ethereum.org/category/ecosystem-support-program/"
+        >
+          Blog
+        </Link>
+        <Bar>|</Bar>
+        <Link
+          className="copy"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://ethereum.org/privacy-policy/"
+        >
+          Privacy Policy
+        </Link>
+        <Bar>|</Bar>
+        <Link
+          className="copy"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://ethereum.org/cookie-policy/"
+        >
+          Cookie Policy
+        </Link>
+      </LinkContainer>
+      <IconContainer>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://twitter.com/EF_ESP"
+        >
+          <Icon icon={faTwitter} />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/ethereum"
+        >
+          <Icon icon={faGithub} />
+        </a>
+      </IconContainer>
+      <Legal>
+        © {new Date().getFullYear()} Ethereum Foundation. <BR />
+        All rights reserved.
+      </Legal>
+    </FooterElement>
+  </>
 )
 
 export default Footer
