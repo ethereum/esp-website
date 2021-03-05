b3edb07eeffa7a71124a9e624dc9d7a259e815b6
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2021-03-05 12:30:28 -0800

    revert LocalGrantsForm changes

--- src/components/LocalGrantsForm.js
+++ src/components/LocalGrantsForm.js
@@ -75,7 +75,7 @@ const requiredFields = [
   "challenges",
   "individualOrTeam",
   "teamProfile",
-  "lastName",
+  "name",
   "contactEmail",
   "referralSource",
 ]
@@ -95,7 +95,7 @@ const LocalGrantsForm = ({ wave }) => {
     challenges: "",
     individualOrTeam: "",
     teamProfile: "",
-    lastName: "",
+    name: "",
     contactEmail: "",
     contactAlternative: "",
     city: "",
@@ -338,7 +338,7 @@ const LocalGrantsForm = ({ wave }) => {
         <Input
           type="text"
           name="name"
-          value={formState.lastName}
+          value={formState.name}
           onChange={handleInputChange}
           maxLength="150"
         />
