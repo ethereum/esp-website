f9be0f501e976b8b45587baa518265c198d88ed5
Nicolás Quiroz <nh.quiroz@gmail.com>
2021-12-27 21:35:41 -0300

    chore: move hamburguer icon to /icons

--- src/components/UI/NavMobile.tsx
+++ src/components/UI/NavMobile.tsx
@@ -1,14 +1,8 @@
-import { createIcon } from '@chakra-ui/icons';
 import { Box, Flex, IconButton, Menu, MenuButton } from '@chakra-ui/react';
 import Image from 'next/image';
 import { FC } from 'react';
 import logoSrc from '../../public/esp-logo.svg';
-
-const HamburgerIcon = createIcon({
-  displayName: 'HamburgerIcon',
-  viewBox: '0 0 32 25',
-  path: <path fill='#F85858' d='M0 0h32v3H0zM0 11h32v3H0zM0 22h32v3H0z' />
-});
+import { HamburgerIcon } from './icons';
 
 export const NavMobile: FC = () => {
   return (
@@ -18,7 +12,8 @@ export const NavMobile: FC = () => {
       </Box>
 
       <Box mt={2}>
-        <Menu>
+        {/* https://github.com/chakra-ui/chakra-ui/issues/3020#issuecomment-916560715 */}
+        <Menu id='menu-button'>
           <MenuButton
             as={IconButton}
             aria-label='Menu'
---
+++ src/components/UI/icons/index.ts
@@ -0,0 +1,1 @@
+export { HamburgerIcon } from './HamburguerIcon';
