b7319953a33c6887053b9b2a723bbe27eaa307ee
Nicolás Quiroz <nh.quiroz@gmail.com>
2021-12-30 11:57:42 -0300

    chore: add custom css to override theme's heading font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import '@fontsource/libre-franklin/200.css';
 import '@fontsource/libre-franklin/300.css';
 import '@fontsource/libre-franklin/400.css';
-import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import '../global.css';
 import theme from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
