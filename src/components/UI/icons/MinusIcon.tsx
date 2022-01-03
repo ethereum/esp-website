be2e1935f614c61d77e27669d16645dc9c38e74a
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-01-03 02:29:42 -0300

    chore: add PlusIcon & MinusIcon

---
+++ src/components/UI/icons/MinusIcon.tsx
@@ -0,0 +1,7 @@
+import { createIcon } from '@chakra-ui/icons';
+
+export const MinusIcon = createIcon({
+  displayName: 'MinusIcon',
+  viewBox: '0 0 20 4',
+  path: <path fill='#f87045' d='M0 0.5H20V3.5H0V0.5Z' />
+});
