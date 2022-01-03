4aa802d950ee31218986a1b3f200d5470c9bb431
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-01-03 02:30:10 -0300

    feat: add FAQItem component

---
+++ src/components/UI/FAQItem.tsx
@@ -0,0 +1,44 @@
+import { AccordionButton, AccordionItem, AccordionPanel, Box, Flex } from '@chakra-ui/react';
+import { FC } from 'react';
+import { MinusIcon, PlusIcon } from '../../components/UI/icons';
+import { FAQQuestion } from './headings';
+
+interface Props {
+  question: string;
+}
+
+export const FAQItem: FC<Props> = ({ question, children }) => {
+  return (
+    <AccordionItem
+      py={2}
+      maxW='100%'
+      borderRadius='10px'
+      border='none'
+      bgGradient='linear(to-r, #ffe5e2 10%, #f3d1ee 100%)'
+      mb='10px'
+    >
+      {({ isExpanded }) => (
+        <>
+          <AccordionButton>
+            {isExpanded ? (
+              <MinusIcon color='brand.orange.200' w={5} h={5} />
+            ) : (
+              <PlusIcon fill='brand.orange.200' w={5} h={5} />
+            )}
+            <Box flex='1' pl='13px'>
+              <FAQQuestion>{question}</FAQQuestion>
+            </Box>
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            <Flex ml={8}>
+              <Box mr={3}>
+                <FAQQuestion>A.</FAQQuestion>
+              </Box>
+              <Box maxW='92%'>{children}</Box>
+            </Flex>
+          </AccordionPanel>
+        </>
+      )}
+    </AccordionItem>
+  );
+};
