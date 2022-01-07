b2f4eddb14f110f0f03b892f8557daa839481c2b
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-01-07 12:02:45 -0300

    feat: add Applicants Context Provider to Layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,17 @@
 import { Box, Container, ContainerProps, Flex, Stack } from '@chakra-ui/react';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useRouter } from 'next/router';
 
 import { Footer, NavMobile, NewsletterSignup, UnicornSpace } from '../UI/common';
 import { ApplicantsLayout } from './ApplicantsLayout';
 
+import { ApplicantsContext } from '../../contexts';
+
+import { APPLICANTS_URL } from '../../constants';
+
 export const Layout: FC<ContainerProps> = ({ children, ...props }) => {
+  const [currentTab, setCurrentTab] = useState(0);
+  const store = { currentTab, setCurrentTab };
   const router = useRouter();
 
   return (
@@ -14,17 +20,19 @@ export const Layout: FC<ContainerProps> = ({ children, ...props }) => {
         <NavMobile />
       </Box>
 
-      {router.pathname.startsWith('/applicants') ? (
+      {router.pathname.startsWith(APPLICANTS_URL) ? (
         <Box mt={-6}>
           <main>
-            <ApplicantsLayout>{children}</ApplicantsLayout>
+            <ApplicantsContext.Provider value={store}>
+              <ApplicantsLayout>{children}</ApplicantsLayout>
+            </ApplicantsContext.Provider>
           </main>
         </Box>
       ) : (
         children
       )}
 
-      <Box px={5} py={3}>
+      <Box px={5} pb={3}>
         <UnicornSpace />
       </Box>
 
