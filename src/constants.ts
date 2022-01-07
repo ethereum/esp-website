cc695351a9aa5e4eb6ff3e84080fef6327c3937a
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-01-07 11:58:29 -0300

    chore: update constants

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,7 @@ export const WHAT_WE_SUPPORT_URL = '/about/what-we-support';
 
 // applicants
 export const APPLICANTS_URL = '/applicants';
+export const APPLICANTS_PAGES_BASEPATH = '/applicants/';
 export const OFFICE_HOURS_URL = '/applicants/office-hours';
 export const SMALL_GRANTS_URL = '/applicants/small-grants';
 export const PROJECT_GRANTS_URL = '/applicants/project-grants';
