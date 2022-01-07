1c07d6126f64c240de77a1dc4817482a6f033297
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-01-07 01:46:42 -0300

    chore: add constants for internal URL strings

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+// about
+export const ABOUT_URL = '/about';
+export const WHAT_WE_SUPPORT_URL = '/about/what-we-support';
+
+// applicants
+export const APPLICANTS_URL = '/applicants';
+export const OFFICE_HOURS_URL = '/applicants/office-hours';
+export const SMALL_GRANTS_URL = '/applicants/small-grants';
+export const PROJECT_GRANTS_URL = '/applicants/project-grants';
