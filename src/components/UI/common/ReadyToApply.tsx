c461ed5f20c11d73932a27bcb0a3dd5546161cc0
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-01-07 15:29:52 -0300

    chore: use a tag instead of button for apply link (#122)

--- src/components/UI/common/ReadyToApply.tsx
+++ src/components/UI/common/ReadyToApply.tsx
@@ -1,6 +1,5 @@
-import { Center, Flex, Heading, Stack } from '@chakra-ui/react';
+import { Center, Flex, Heading, Link, Stack } from '@chakra-ui/react';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
 import { FC } from 'react';
 
 import { ImportantText } from '../headings';
@@ -12,8 +11,6 @@ interface Props {
 }
 
 export const ReadyToApply: FC<Props> = ({ link }) => {
-  const router = useRouter();
-
   return (
     <Stack
       borderRadius='20px'
@@ -36,23 +33,23 @@ export const ReadyToApply: FC<Props> = ({ link }) => {
       </Stack>
 
       <Center>
-        <Flex
-          as='button'
-          _hover={{ bg: 'brand.hover' }}
-          backgroundColor='brand.accent'
-          w='208px'
-          py={4}
-          justifyContent='center'
-          alignItems='center'
-          position='relative'
-          onClick={() => router.push(link)}
-        >
-          <ImportantText color='white'>Apply</ImportantText>
+        <Link href={link} _hover={{ textDecoration: 'none' }}>
+          <Flex
+            _hover={{ bg: 'brand.hover' }}
+            backgroundColor='brand.accent'
+            w='208px'
+            py={4}
+            justifyContent='center'
+            alignItems='center'
+            position='relative'
+          >
+            <ImportantText color='white'>Apply</ImportantText>
 
-          <Flex position='absolute' left={36}>
-            <Image src={planeVectorSVG} alt='paper plane vector' height='29px' width='32px' />
+            <Flex position='absolute' left={36}>
+              <Image src={planeVectorSVG} alt='paper plane vector' height='29px' width='32px' />
+            </Flex>
           </Flex>
-        </Flex>
+        </Link>
       </Center>
     </Stack>
   );
