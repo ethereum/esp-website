3e8f50b4e02a2eaa4e0f5f03894e7c672b99da88
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-01-07 01:48:05 -0300

    chore: update Layout with ApplicantsLayout conditional rendering

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,30 @@
-import { Box, Container, ContainerProps } from '@chakra-ui/react';
+import { Box, Container, ContainerProps, Flex, Stack } from '@chakra-ui/react';
 import { FC } from 'react';
+import { useRouter } from 'next/router';
 
 import { Footer, NavMobile, NewsletterSignup, UnicornSpace } from '../UI/common';
+import { ApplicantsLayout } from './ApplicantsLayout';
 
 export const Layout: FC<ContainerProps> = ({ children, ...props }) => {
+  const router = useRouter();
+
   return (
     <Container maxW={{ base: 'container.mobile', md: '600px' }} p={0} {...props}>
       <Box px={5} py={3}>
         <NavMobile />
+      </Box>
 
-        {children}
+      {router.pathname.startsWith('/applicants') ? (
+        <Box mt={-6}>
+          <main>
+            <ApplicantsLayout>{children}</ApplicantsLayout>
+          </main>
+        </Box>
+      ) : (
+        children
+      )}
 
+      <Box px={5} py={3}>
         <UnicornSpace />
       </Box>
 
