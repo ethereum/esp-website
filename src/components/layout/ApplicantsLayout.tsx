75c83268026bb319306dc4c4ff6db82c307ccc89
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-01-07 01:40:40 -0300

    feat: add tab navigation to ApplicantsLayout

--- src/components/layout/ApplicantsLayout.tsx
+++ src/components/layout/ApplicantsLayout.tsx
@@ -1,27 +1,170 @@
-import { Stack } from '@chakra-ui/react';
-import { FC } from 'react';
-import { ApplicantsDescription, ReadyToApply } from '../UI/common';
+import { Box, Flex, Stack, Tab, TabList, Tabs } from '@chakra-ui/react';
+import { FC, useState } from 'react';
+
+import { ApplicantsDescription, ImportantText, ReadyToApply } from '../UI';
+
+import {
+  APPLICANTS_URL,
+  OFFICE_HOURS_URL,
+  PROJECT_GRANTS_URL,
+  SMALL_GRANTS_URL
+} from '../../constants';
 
 interface Props {
-  linkToApply: string;
+  idx: number;
 }
 
-export const ApplicantsLayout: FC<Props> = ({ children, linkToApply }) => {
+import { useRouter } from 'next/router';
+
+export const ApplicantsLayout: FC = ({ children }) => {
+  // use context api value here
+  const [tabIndex, setTabIndex] = useState(0);
+  const router = useRouter();
+
+  const handleChange = (index: number) => {
+    setTabIndex(index);
+
+    switch (index) {
+      case 0:
+        router.push(
+          {
+            pathname: APPLICANTS_URL
+          },
+          undefined,
+          { scroll: false }
+        );
+        break;
+
+      case 1:
+        router.push(
+          {
+            pathname: OFFICE_HOURS_URL
+          },
+          undefined,
+          { scroll: false }
+        );
+        break;
+
+      case 2:
+        router.push(
+          {
+            pathname: SMALL_GRANTS_URL
+          },
+          undefined,
+          { scroll: false }
+        );
+        break;
+
+      case 3:
+        router.push(
+          {
+            pathname: PROJECT_GRANTS_URL
+          },
+          undefined,
+          { scroll: false }
+        );
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
-    <main>
-      <Stack mb={32}>
+    <>
+      <Stack mb={5} px={5} py={3}>
         <section id='hero'>
           <ApplicantsDescription />
         </section>
       </Stack>
 
+      <Flex
+        mb={10}
+        backgroundColor='white'
+        overflowX='auto'
+        borderBottom='1px solid'
+        borderBottomColor='brand.divider'
+      >
+        <Tabs
+          id='tabbed-navigation'
+          align='center'
+          index={tabIndex}
+          onChange={index => handleChange(index)}
+          variant='unstyled'
+          isLazy
+        >
+          <TabList whiteSpace='nowrap' h='64px'>
+            <Tab
+              px={0}
+              mx={6}
+              pb={0}
+              borderBottom='10px solid'
+              borderBottomColor='transparent'
+              _selected={{
+                borderBottom: '10px solid',
+                borderBottomColor: 'brand.accent'
+              }}
+            >
+              <ImportantText color='brand.ready.text' display='inline'>
+                Overview
+              </ImportantText>
+            </Tab>
+            <Tab
+              px={0}
+              mx={6}
+              pb={0}
+              borderBottom='10px solid'
+              borderBottomColor='transparent'
+              _selected={{
+                borderBottom: '10px solid',
+                borderBottomColor: 'brand.accent'
+              }}
+            >
+              <ImportantText color='brand.ready.text' display='inline'>
+                Office Hours
+              </ImportantText>
+            </Tab>
+            <Tab
+              px={0}
+              mx={6}
+              pb={0}
+              borderBottom='10px solid'
+              borderBottomColor='transparent'
+              _selected={{
+                borderBottom: '10px solid',
+                borderBottomColor: 'brand.accent'
+              }}
+            >
+              <ImportantText color='brand.ready.text' display='inline'>
+                Small Grants
+              </ImportantText>
+            </Tab>
+            <Tab
+              px={0}
+              mx={6}
+              pb={0}
+              borderBottom='10px solid'
+              borderBottomColor='transparent'
+              _selected={{
+                borderBottom: '10px solid',
+                borderBottomColor: 'brand.accent'
+              }}
+            >
+              <ImportantText color='brand.ready.text' display='inline'>
+                Project Grants
+              </ImportantText>
+            </Tab>
+          </TabList>
+        </Tabs>
+      </Flex>
+
       {children}
 
       <Stack>
         <section id='ready-to-apply'>
-          <ReadyToApply link={linkToApply} />
+          <ReadyToApply link={'#'} />
         </section>
       </Stack>
-    </main>
+    </>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import { Layout } from '../components/UI';
+import { Layout } from '../components/layout';
 
 import theme from '../theme';
 
