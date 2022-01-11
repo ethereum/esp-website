f25c3a9f2b3da3e847a63b4af011e82efd281fc1
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-01-10 21:50:09 -0300

    feat: add hover animation to Apply button

--- src/components/UI/common/ReadyToApply.tsx
+++ src/components/UI/common/ReadyToApply.tsx
@@ -1,6 +1,7 @@
-import { Center, Flex, Heading, Link, Stack } from '@chakra-ui/react';
+import { Box, BoxProps, Center, Flex, FlexProps, Heading, Link, Stack } from '@chakra-ui/react';
+import { motion, useAnimation } from 'framer-motion';
 import Image from 'next/image';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 import { ImportantText } from '../headings';
 
@@ -10,7 +11,27 @@ interface Props {
   link: string;
 }
 
+const MotionBox = motion<BoxProps>(Box);
+const MotionFlex = motion<FlexProps>(Flex);
+
 export const ReadyToApply: FC<Props> = ({ link }) => {
+  const [buttonHovered, setbButtonHovered] = useState(false);
+  const shadowBox = useAnimation();
+
+  useEffect(() => {
+    console.log({ buttonHovered });
+
+    const sequence = async () => {
+      if (buttonHovered) {
+        shadowBox.start({ x: 1.5, y: 1.5 });
+      } else {
+        shadowBox.start({ x: 0, y: 0 });
+      }
+    };
+
+    sequence();
+  }, [buttonHovered, shadowBox]);
+
   return (
     <Stack
       borderRadius='20px'
@@ -34,21 +55,37 @@ export const ReadyToApply: FC<Props> = ({ link }) => {
 
       <Center>
         <Link href={link} _hover={{ textDecoration: 'none' }}>
-          <Flex
-            _hover={{ bg: 'brand.hover' }}
-            backgroundColor='brand.accent'
-            w='208px'
-            py={4}
-            justifyContent='center'
-            alignItems='center'
-            position='relative'
-          >
-            <ImportantText color='white'>Apply</ImportantText>
+          <Box position='relative'>
+            <MotionBox
+              backgroundColor='brand.shadow'
+              h='56px'
+              w='208px'
+              py={4}
+              justifyContent='center'
+              alignItems='center'
+              position='absolute'
+              animate={shadowBox}
+            />
 
-            <Flex position='absolute' left={36}>
-              <Image src={planeVectorSVG} alt='paper plane vector' height='29px' width='32px' />
-            </Flex>
-          </Flex>
+            <MotionFlex
+              backgroundColor='brand.accent'
+              w='208px'
+              py={4}
+              justifyContent='center'
+              alignItems='center'
+              position='relative'
+              _hover={{ bg: 'brand.hover' }}
+              onMouseEnter={() => setbButtonHovered(true)}
+              onMouseLeave={() => setbButtonHovered(false)}
+              whileHover={{ x: -1.5, y: -1.5 }}
+            >
+              <ImportantText color='white'>Apply</ImportantText>
+
+              <Flex position='absolute' left={36}>
+                <Image src={planeVectorSVG} alt='paper plane vector' height='29px' width='32px' />
+              </Flex>
+            </MotionFlex>
+          </Box>
         </Link>
       </Center>
     </Stack>
