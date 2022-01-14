c4dae6824c9cbfd821f31c96acbef1d990bbeac4
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-01-14 13:24:08 -0300

    chore: update form data types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,27 @@
 export interface TabsMap {
   [name: string]: number;
 }
+
+export type NewsletterFormData = {
+  email: string;
+};
+
+export type ProjectGrantsFormData = {
+  projectName: string;
+  organizationName: string;
+  website: string;
+  github: string;
+  twitter: string;
+  teamProfile: string;
+  projectSummary: string;
+  projectCategory: string;
+  requestedAmount: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  city: string;
+  country: string;
+  timezone: string;
+  howDidYouHearAboutESP: string;
+  recommendation: string;
+};
