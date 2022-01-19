582124ec77847fd6cace0a73495b363ed01a1bbb
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-01-19 18:57:14 -0300

    chore: update types.ts

--- src/types.ts
+++ src/types.ts
@@ -25,3 +25,30 @@ export type ProjectGrantsFormData = {
   howDidYouHearAboutESP: string;
   recommendation: string;
 };
+
+export type OfficeHoursFormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  individualOrTeam: string;
+  company: string;
+  projectName: string;
+  projectSummary: string;
+  additionalInfo: string;
+  projectCategory: string;
+  howDidYouHearAboutESP: string;
+  reasonForMeeting: string;
+  otherReasonForMeeting: string;
+  timezone: string;
+};
+
+export type IndividualOrTeam = 'Individual' | 'Team';
+
+export type ReasonForMeeting = [
+  | 'Project feedback or advice'
+  | 'Questions about ESP'
+  | 'Questions about applying for a grant'
+  | 'How to contribute to Ethereum'
+  | 'Other'
+  | ''
+];
