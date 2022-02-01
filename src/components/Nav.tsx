a1abdb814778bf1a411b950c43a041be59cb29b7
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-02-01 18:54:43 -0300

    feat: update Nav component for desktop

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,21 +8,27 @@ import {
   Flex,
   IconButton,
   Link,
+  ListIcon,
+  ListItem,
   Menu,
   MenuButton,
   Stack,
+  UnorderedList,
   useDisclosure
 } from '@chakra-ui/react';
 import Image from 'next/image';
+import NextLink from 'next/link';
 import { useRouter } from 'next/router';
 import { FC } from 'react';
 
 import { ImportantText } from './UI/headings';
-import { CloseIcon, HamburgerIcon } from './UI/icons';
+import { CloseIcon, HamburgerIcon, NavLinkIcon } from './UI/icons';
+
+import { selectedLink } from '../utils';
 
 import logoSVG from '../public/images/esp-logo.svg';
 
-import { ETHEREUM_ORG_URL, MOBILE_MENU } from '../constants';
+import { ESP_BLOG_URL, ETHEREUM_ORG_URL, HOME_URL, NAV_LINKS } from '../constants';
 
 export const Nav: FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -32,7 +38,7 @@ export const Nav: FC = () => {
     <header>
       <Flex justifyContent='space-between' mb={12}>
         <Box
-          onClick={isOpen ? undefined : () => router.push('/')}
+          onClick={isOpen ? undefined : () => router.push(HOME_URL)}
           cursor={isOpen ? 'default' : 'pointer'}
           sx={isOpen ? { transitionDelay: '0.15s', filter: 'brightness(10)' } : undefined}
           zIndex={9999}
@@ -71,7 +77,7 @@ export const Nav: FC = () => {
               <DrawerBody bgGradient='linear(to-br, brand.footer.bgGradient.start 10%, brand.footer.bgGradient.end 100%)'>
                 <Stack h='100%' justifyContent='center' alignItems='center'>
                   <Stack spacing={6}>
-                    {MOBILE_MENU.map(({ href, text }) => (
+                    {NAV_LINKS.map(({ href, text }) => (
                       <Link key={href} href={href} _hover={{ textDecoration: 'none' }}>
                         <ImportantText color='white' textAlign='center' fontSize='h2'>
                           {text}
@@ -94,6 +100,37 @@ export const Nav: FC = () => {
             </DrawerContent>
           </Drawer>
         </Box>
+
+        <Flex display={{ base: 'none', md: 'block' }}>
+          <UnorderedList>
+            {NAV_LINKS.map((nav, idx) => (
+              <ListItem key={nav.text} display='inline'>
+                {idx > 0 && <ListIcon as={NavLinkIcon} display='inline-block' pb={1} />}
+                <NextLink href={nav.href}>
+                  <Link
+                    href={nav.href}
+                    target={nav.href === ESP_BLOG_URL ? '_blank' : '_self'}
+                    rel='noopener noreferrer'
+                    _hover={{ textDecoration: 'none' }}
+                  >
+                    <ImportantText
+                      color='brand.ready.text'
+                      display='inline'
+                      mr={2}
+                      sx={
+                        selectedLink(router.pathname, nav.href)
+                          ? { borderBottom: '10px solid', borderBottomColor: 'brand.accent' }
+                          : undefined
+                      }
+                    >
+                      {nav.text}
+                    </ImportantText>
+                  </Link>
+                </NextLink>
+              </ListItem>
+            ))}
+          </UnorderedList>
+        </Flex>
       </Flex>
     </header>
   );
