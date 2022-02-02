f474bdf130b36e6e7ed9fa8cda66b3561cf1dad3
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-02-02 18:35:08 -0300

    chore: update HomepageHero background image

--- src/components/UI/HomepageHero.tsx
+++ src/components/UI/HomepageHero.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Stack, useMediaQuery } from '@chakra-ui/react';
+import { Box, Center, Flex, Img, Stack } from '@chakra-ui/react';
 import Image from 'next/image';
 import { FC } from 'react';
 
@@ -11,34 +11,58 @@ import homepageHeroMobile from '../../public/images/homepage-hero-mobile.svg';
 import { APPLICANTS_URL } from '../../constants';
 
 export const HomepageHero: FC = () => {
-  const [isMobile] = useMediaQuery('(max-width: 768px)');
-
   return (
     <Stack mb={6}>
-      <Stack px={5} py={3} mb={-12} zIndex={1}>
+      <Stack
+        px={{ base: 5, md: 9 }}
+        py={{ base: 3, md: 12 }}
+        mb={{ base: -12, md: 5, lg: 52 }}
+        bg={{ md: 'rgba(255, 255, 255, 0.6)' }}
+        ml={{ md: 12 }}
+        maxW={{ md: '626px' }}
+        zIndex={1}
+      >
         <PageSubheading
           as='h1'
-          fontSize='homepage'
+          fontSize={{ base: 'homepage', md: 'h1' }}
           fontWeight={100}
-          lineHeight='36px'
+          lineHeight={{ base: '36px', md: '48px' }}
           textAlign='left'
-          mb={6}
+          mb={{ base: 6, md: 10 }}
         >
           We provide grants and other support to the builders of the Ethereum ecosystem.
         </PageSubheading>
 
-        <Center>
+        <Flex justifyContent={{ base: 'center', md: 'flex-start' }}>
           <ButtonLink label='Learn more' link={APPLICANTS_URL} width='177px' />
-        </Center>
+        </Flex>
       </Stack>
 
       <Box>
-        <Image
-          src={isMobile ? homepageHeroMobile : homepageHero}
-          alt='People gathered around the Ethereum tree'
-          objectFit='cover'
-          quality={85}
-        />
+        <Box display={{ base: 'block', md: 'none' }}>
+          <Image
+            src={homepageHeroMobile}
+            alt='People gathered around the Ethereum tree'
+            layout='responsive'
+            objectFit='cover'
+            quality={85}
+            priority={true}
+          />
+        </Box>
+
+        <Box display={{ base: 'none', md: 'block' }} mt={6}>
+          <Image
+            src={homepageHero}
+            alt='People gathered around the Ethereum tree'
+            // width='1378px'
+            // height='600px'
+            // className='background-image'
+            layout='fill'
+            objectFit='cover'
+            quality={85}
+            priority={true}
+          />
+        </Box>
       </Box>
     </Stack>
   );
