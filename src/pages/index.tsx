c17e80469c1709933b53e1e67f9448c9f9f72b7a
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-02-02 16:49:46 -0300

    feat: add HomeAbout cards to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,21 @@
-import { Box, Container, Flex, Stack, useMediaQuery } from '@chakra-ui/react';
+import { Box, Flex, Stack } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Image from 'next/image';
 import Head from 'next/head';
 
 import { ButtonLink } from '../components';
-import { PageSection, PageText } from '../components/UI';
+import { HomeAboutCard, PageSection, PageText } from '../components/UI';
 
 import smallSucculentSVG from '../public/images/small-succulent.svg';
 import mediumSucculentSVG from '../public/images/medium-succulent.svg';
 import bigSucculentSVG from '../public/images/big-succulent.svg';
-import whatWeSupportTreeMobileSVG from '../public/images/what-we-support-tree-mobile.svg';
 import whatWeSupportTreeSVG from '../public/images/what-we-support-tree.svg';
-import whoWeSupportRootsMobileSVG from '../public/images/who-we-support-roots-mobile.svg';
 import whoWeSupportRootsSVG from '../public/images/who-we-support-roots.svg';
-import howWeSupportRootsMobileSVG from '../public/images/how-we-support-roots-mobile.svg';
 import howWeSupportRootsSVG from '../public/images/how-we-support-roots.svg';
 
 import { ABOUT_URL, HOW_WE_SUPPORT_URL, WHO_WE_SUPPORT_URL } from '../constants';
 
 const Home: NextPage = () => {
-  const [isMobile] = useMediaQuery('(max-width: 768px)');
-
   return (
     <>
       <Head>
@@ -194,149 +189,57 @@ const Home: NextPage = () => {
           </section>
 
           <section id='what-we-support'>
-            <Stack
-              borderRadius='10px'
+            <HomeAboutCard
               bgGradient='linear(to-br, brand.ready.bgGradient.start 10%, brand.ready.bgGradient.end 100%)'
-              h={{ xs: '100%', md: '458px' }}
-              w='100%'
-              justifyContent='center'
+              img={{ src: whatWeSupportTreeSVG, alt: 'supported categories tree' }}
+              title='What we support'
+              link={ABOUT_URL}
             >
-              <Flex
-                alignItems='center'
-                direction={{ base: 'column', md: 'row' }}
-                px={6}
-                py={{ xs: 10, md: 0 }}
-              >
-                <Stack mb={4} mr={{ base: 0, md: 12 }}>
-                  <PageSection textAlign={{ base: 'center', md: 'left' }} mb={6}>
-                    What we support
-                  </PageSection>
-
-                  <Box>
-                    <Image
-                      src={isMobile ? whatWeSupportTreeMobileSVG : whatWeSupportTreeSVG}
-                      alt='supported categories tree'
-                      objectFit='cover'
-                      quality={85}
-                    />
-                  </Box>
-                </Stack>
-
-                <Stack>
-                  <PageText mb={6}>
-                    We award dozens of grants each quarter, across a variety of categories. We
-                    support <strong>open source projects</strong> that strengthen Ethereum&apos;s
-                    foundations, with a particular focus on builder tools, infrastructure, research,
-                    community resources and other public goods. Funding is generally directed toward
-                    supporting builders rather than end users.
-                  </PageText>
-
-                  <Flex justifyContent={{ base: 'center', md: 'flex-start' }}>
-                    <ButtonLink label='Read more' link={ABOUT_URL} width='247px' />
-                  </Flex>
-                </Stack>
-              </Flex>
-            </Stack>
+              We award dozens of grants each quarter, across a variety of categories. We support{' '}
+              <strong>open source projects</strong> that strengthen Ethereum&apos;s foundations,
+              with a particular focus on builder tools, infrastructure, research, community
+              resources and other public goods. Funding is generally directed toward supporting
+              builders rather than end users.
+            </HomeAboutCard>
           </section>
 
           <section id='who-we-support'>
-            <Stack
-              borderRadius='10px'
+            <HomeAboutCard
               bgGradient='linear(to-br, brand.whoWeSupport.bgGradient.start 0%, brand.whoWeSupport.bgGradient.end 100%)'
-              h={{ xs: '100%', md: '458px' }}
-              w='100%'
-              justifyContent='center'
+              img={{ src: whoWeSupportRootsSVG, alt: 'supported categories tree' }}
+              title='Who We Support'
+              link={WHO_WE_SUPPORT_URL}
             >
-              <Flex
-                alignItems='center'
-                direction={{ base: 'column', md: 'row' }}
-                px={6}
-                py={{ xs: 10, md: 0 }}
-              >
-                <Stack mb={4} mr={{ base: 0, md: 12 }}>
-                  <PageSection textAlign={{ base: 'center', md: 'left' }} mb={6}>
-                    Who We Support
-                  </PageSection>
-
-                  <Box>
-                    <Image
-                      src={isMobile ? whoWeSupportRootsMobileSVG : whoWeSupportRootsSVG}
-                      alt='supported categories tree'
-                      objectFit='cover'
-                      quality={85}
-                    />
-                  </Box>
-                </Stack>
-
-                <Stack>
-                  <PageText mb={6}>
-                    Our grantees come from all over the world and represent many different
-                    backgrounds, disciplines and levels of experience. We have supported individuals
-                    and teams of all kinds - companies, DAOs, nonprofits, institutions, academics,
-                    developers, educators, community organizers and more. Learn more and see a list
-                    of recent grantees.
-                  </PageText>
-
-                  <Flex justifyContent={{ base: 'center', md: 'flex-start' }}>
-                    <ButtonLink label='Read more' link={WHO_WE_SUPPORT_URL} width='247px' />
-                  </Flex>
-                </Stack>
-              </Flex>
-            </Stack>
+              Our grantees come from all over the world and represent many different backgrounds,
+              disciplines and levels of experience. We have supported individuals and teams of all
+              kinds - companies, DAOs, nonprofits, institutions, academics, developers, educators,
+              community organizers and more. Learn more and see a list of recent grantees.
+            </HomeAboutCard>
           </section>
 
           <section id='how-we-support'>
-            <Stack
-              borderRadius='10px'
+            <HomeAboutCard
               bgGradient='linear(to-br, brand.howWeSupport.bgGradient.start 2.29%, brand.howWeSupport.bgGradient.end 101.37%)'
-              h={{ xs: '100%', md: '458px' }}
-              w='100%'
-              justifyContent='center'
+              img={{
+                src: howWeSupportRootsSVG,
+                alt: 'people making research about supported categories'
+              }}
+              title='How We Support'
+              link={HOW_WE_SUPPORT_URL}
             >
-              <Flex
-                alignItems='center'
-                direction={{ base: 'column', md: 'row' }}
-                px={6}
-                py={{ xs: 10, md: 0 }}
-              >
-                <Stack mb={4} mr={{ base: 0, md: 12 }}>
-                  <PageSection textAlign={{ base: 'center', md: 'left' }} mb={6}>
-                    How We Support
-                  </PageSection>
+              <PageText mb={6}>
+                We aim to deploy resources, both financial and non-financial, where they will have
+                the biggest impact. We try to keep our processes flexible and evolving in order to
+                be open to new ideas and support builders of all kinds.
+              </PageText>
 
-                  <Box>
-                    <Image
-                      src={isMobile ? howWeSupportRootsMobileSVG : howWeSupportRootsSVG}
-                      alt='people making research about supported categories'
-                      objectFit='cover'
-                      quality={85}
-                    />
-                  </Box>
-                </Stack>
-
-                <Stack>
-                  <Stack mb={6}>
-                    <PageText mb={6}>
-                      We aim to deploy resources, both financial and non-financial, where they will
-                      have the biggest impact. We try to keep our processes flexible and evolving in
-                      order to be open to new ideas and support builders of all kinds.
-                    </PageText>
-
-                    <PageText>
-                      Grants are awarded through a formal application and evaluation process, while
-                      our office hours are an opportunity to explore a broad range of support
-                      through an informal conversation with a member of the ESP team, such as
-                      project feedback, advice, help navigating the ecosystem or guidance on
-                      submitting a grant application.
-                    </PageText>
-                  </Stack>
-
-                  <Flex justifyContent={{ base: 'center', md: 'flex-start' }}>
-                    <ButtonLink label='Read more' link={HOW_WE_SUPPORT_URL} width='247px' />
-                  </Flex>
-                </Stack>
-              </Flex>
-            </Stack>
+              <PageText>
+                Grants are awarded through a formal application and evaluation process, while our
+                office hours are an opportunity to explore a broad range of support through an
+                informal conversation with a member of the ESP team, such as project feedback,
+                advice, help navigating the ecosystem or guidance on submitting a grant application.
+              </PageText>
+            </HomeAboutCard>
           </section>
         </Stack>
       </Box>
