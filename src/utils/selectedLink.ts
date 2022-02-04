8402b5561c7c26f2e48d048a72e73fa3d0730b69
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-02-04 13:25:48 -0300

    feat: update /applicants layout for desktop

--- src/utils/selectedLink.ts
+++ src/utils/selectedLink.ts
@@ -1,4 +1,4 @@
-import { ABOUT_URL, APPLICANTS_URL, ESP_BLOG_URL, HOME_URL } from '../constants';
+import { ABOUT_URL, APPLICANTS_URL, HOME_URL } from '../constants';
 
 export const selectedLink = (pathname: string, href: string) =>
   (pathname === HOME_URL && href === HOME_URL) ||
