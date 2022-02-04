b506511b87555eca0a26a1462f88231e5d875b39
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-02-04 16:33:34 -0300

    chore: update /applicants hero for desktop

--- src/components/UI/common/ApplicantsDescription.tsx
+++ src/components/UI/common/ApplicantsDescription.tsx
@@ -1,23 +1,35 @@
-import { Center } from '@chakra-ui/react';
+import { Box, Center, Flex } from '@chakra-ui/react';
 import { FC } from 'react';
+import Image from 'next/image';
 
 import { PageHeading } from '../headings';
-import { PlaceholderImage } from '../PlaceholderImage';
 import { PageText } from '../text';
+
+import applicantsHero from '../../../public/images/applicants-hero.svg';
 
 export const ApplicantsDescription: FC = () => {
   return (
-    <>
-      <PageHeading mb={4} ml={-1}>
-        For Applicants
-      </PageHeading>
-      <PageText mb={2}>
-        Whether you&apos;re working on a specific project, or you&apos;re still exploring
-        possibilities, you can connect with our team for guidance.
-      </PageText>
-      <Center>
-        <PlaceholderImage height='250px' width='360px' />
-      </Center>
-    </>
+    <Flex direction={{ base: 'column', md: 'row' }}>
+      <Box mr={{ md: 40 }} w='100%'>
+        <PageHeading mb={4} ml={-1}>
+          For Applicants
+        </PageHeading>
+
+        <PageText mb={2}>
+          Whether you&apos;re working on a specific project, or you&apos;re still exploring
+          possibilities, you can connect with our team for guidance.
+        </PageText>
+      </Box>
+
+      <Box mt={{ base: 8, md: -20 }} w='100%'>
+        <Image
+          src={applicantsHero}
+          alt='Kid watching plants grow'
+          objectFit='cover'
+          quality={85}
+          priority={true}
+        />
+      </Box>
+    </Flex>
   );
 };
