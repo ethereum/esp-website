8246cb8220adc8cfd360a86c6bc4896bc98e01a2
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-02-07 23:01:49 -0300

    chore: use .docx format for document download

--- src/pages/api/download-application.ts
+++ src/pages/api/download-application.ts
@@ -4,7 +4,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const response = await fetch(process.env.PROJECT_GRANTS_DOWNLOAD_FILE_URL!);
   if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
 
-  res.setHeader('Content-Type', 'application/msword');
-  res.setHeader('Content-Disposition', 'attachment; filename=projectGrantsApplication.doc');
+  res.setHeader(
+    'Content-Type',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+  );
+  res.setHeader('Content-Disposition', 'attachment; filename=projectGrantsApplication.docx');
   res.send(response.body);
 }
