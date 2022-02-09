1506ae9271ab00d17c343f54a7fceaae2c4ebc00
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-02-09 19:35:05 -0300

    fix: hero responsiveness

--- src/components/UI/common/Description.tsx
+++ src/components/UI/common/Description.tsx
@@ -18,7 +18,7 @@ export const Description: FC<Props> = ({ title, img, children }) => {
 
   return (
     <Flex direction={{ base: 'column', lg: 'row' }} pr={{ lg: 32 }} mb={1}>
-      <Box ml={{ md: 16 }} mr={{ md: 6, lg: 40 }}>
+      <Box ml={{ md: 16 }} mr={{ lg: 6, xl: 32 }} mb={{ md: 24, lg: 0 }} minWidth={{ lg: 96 }}>
         <PageHeading mb={4} ml={-1}>
           {title}
         </PageHeading>
@@ -26,7 +26,7 @@ export const Description: FC<Props> = ({ title, img, children }) => {
         <PageText mb={2}>{children}</PageText>
       </Box>
 
-      <Box mt={{ base: 8, md: -24 }} mx={{ base: 'auto', md: 0 }} flexShrink={0}>
+      <Box mt={{ base: 8, md: -24 }} mx={{ base: 'auto', lg: 0 }} flexShrink={0}>
         <Image src={src} alt={alt} objectFit='cover' priority={true} />
       </Box>
     </Flex>
