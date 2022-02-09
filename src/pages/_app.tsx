be2e8ed5e079d9fcaa9b2d3d7852830836d59368
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-02-09 17:54:42 -0300

    fix: hero background not showing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,12 +23,11 @@ import { ABOUT_URL, APPLICANTS_URL, HOME_URL } from '../constants';
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
-  const bgGradient =
-    router.pathname === APPLICANTS_URL
-      ? 'linear(to-b, brand.applicants.bgGradient.start 0%, brand.applicants.bgGradient.end 81.77%, brand.applicants.rgba 100%)'
-      : router.pathname === ABOUT_URL
-      ? 'linear(to-b, brand.about.bgGradient.start 0%, brand.about.bgGradient.end 77.6%, brand.about.rgba 100%)'
-      : undefined;
+  const bgGradient = router.pathname.startsWith(APPLICANTS_URL)
+    ? 'linear(to-b, brand.applicants.bgGradient.start 0%, brand.applicants.bgGradient.end 81.77%, brand.applicants.rgba 100%)'
+    : router.pathname.startsWith(ABOUT_URL)
+    ? 'linear(to-b, brand.about.bgGradient.start 0%, brand.about.bgGradient.end 77.6%, brand.about.rgba 100%)'
+    : undefined;
 
   return (
     <>
