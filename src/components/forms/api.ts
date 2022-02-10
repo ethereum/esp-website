5ee1a50fa07afa6f6ec059efca21e12d31c5e0d7
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-02-10 15:39:24 -0300

    feat: add /api abstraction layer for forms POST ops

---
+++ src/components/forms/api.ts
@@ -0,0 +1,23 @@
+import { OfficeHoursFormData } from '../../types';
+
+export const api = {
+  officeHours: {
+    submit: (data: OfficeHoursFormData) => {
+      const officeHoursRequestOptions: RequestInit = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          ...data,
+          projectCategory: data.projectCategory.value,
+          howDidYouHearAboutESP: data.howDidYouHearAboutESP.value,
+          // Multipickist values (reasonForMeeting) in SF are stored in a string that separates each value by a semicolon
+          reasonForMeeting: data.reasonForMeeting.join(';'),
+          timezone: data.timezone.value
+          // TODO: add recordTypeFlag when defined
+        })
+      };
+
+      return fetch('/api/office-hours', officeHoursRequestOptions).then(res => res);
+    }
+  }
+};
