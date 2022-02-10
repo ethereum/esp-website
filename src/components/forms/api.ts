2e1a702152fbfa52aed44e1b6c554df568f4c6b5
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-02-10 16:40:35 -0300

    chore: update /forms/api

--- src/components/forms/api.ts
+++ src/components/forms/api.ts
@@ -10,8 +10,9 @@ export const api = {
           ...data,
           projectCategory: data.projectCategory.value,
           howDidYouHearAboutESP: data.howDidYouHearAboutESP.value,
+          // TODO: enable reasonForMeeting field when available on SF's sandbox
           // Multipickist values (reasonForMeeting) in SF are stored in a string that separates each value by a semicolon
-          reasonForMeeting: data.reasonForMeeting.join(';'),
+          // reasonForMeeting: data.reasonForMeeting.join(';'),
           timezone: data.timezone.value
           // TODO: add recordTypeFlag when defined
         })
