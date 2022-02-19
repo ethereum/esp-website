dee9bdd2d322a25b18a690feda267446a7024a51
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-02-19 19:07:04 -0300

    chore: remove PlaceholderImage component

--- src/pages/applicants/office-hours/thank-you.tsx
+++ src/pages/applicants/office-hours/thank-you.tsx
@@ -1,8 +1,8 @@
-import { Box, Center } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-import { PageMetadata, PlaceholderImage, ThankYouBody } from '../../../components/UI';
+import { PageMetadata, ThankYouBody } from '../../../components/UI';
 
 const OfficeHoursThankYou: NextPage = () => {
   return (
@@ -14,10 +14,6 @@ const OfficeHoursThankYou: NextPage = () => {
 
       <Box bg='white' position='relative' py={{ md: 12 }} px={{ md: 20, lg: 24, xl: 72 }}>
         <ThankYouBody grantType='Office Hours' />
-
-        <Center>
-          <PlaceholderImage height='250px' width='360px' />
-        </Center>
       </Box>
     </>
   );
