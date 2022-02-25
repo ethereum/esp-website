c53df1eb0befba2906f8cb9be4e3abd0660a3a70
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-02-25 16:22:16 -0300

    chore: update thank you page copy

--- src/pages/academic-grants/thank-you.tsx
+++ src/pages/academic-grants/thank-you.tsx
@@ -1,8 +1,9 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Heading, Link, Stack } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-import { PageMetadata, ThankYouBody } from '../../components/UI';
+import { PageMetadata, PageSubheading, PageText } from '../../components/UI';
+import { ACADEMIC_GRANTS_EMAIL_ADDRESS } from '../../constants';
 
 const AcademicGrantsThankYou: NextPage = () => {
   return (
@@ -12,8 +13,41 @@ const AcademicGrantsThankYou: NextPage = () => {
         <meta name='robots' content='noindex' />
       </Head>
 
-      <Box bg='white' position='relative' py={{ md: 12 }} px={{ md: 20, lg: 24, xl: 72 }}>
-        <ThankYouBody grantType='Academic Grants' />
+      <Box bg='white' position='relative' py={{ md: 12 }} px={{ md: 20, xl: 64 }}>
+        <Stack mb={10}>
+          <section id='description'>
+            <Heading
+              as='h1'
+              color='brand.heading'
+              fontSize='h1'
+              fontWeight={100}
+              lineHeight='48px'
+              textAlign='center'
+              mb={4}
+            >
+              Thank you!
+            </Heading>
+
+            <PageSubheading mb={16} textAlign='center'>
+              for applying to Academic Grants
+            </PageSubheading>
+
+            <PageText mb={6} textAlign='center'>
+              You should receive a confirmation email from us soon. If you have any questions in the
+              meantime, you can reach us at{' '}
+              <Link
+                fontWeight={700}
+                color='brand.orange.100'
+                href={ACADEMIC_GRANTS_EMAIL_ADDRESS}
+                isExternal
+                _hover={{ textDecoration: 'none' }}
+              >
+                academics-grants@ethereum.org
+              </Link>
+              .
+            </PageText>
+          </section>
+        </Stack>
       </Box>
     </>
   );
