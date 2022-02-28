a3435009ab57df4f07115d7fd117140f18cc5968
Sam Richards <sbrichards@gmail.com>
2022-02-28 11:18:14 -0800

    Update hero text

--- src/components/UI/AcademicGrantsHero.tsx
+++ src/components/UI/AcademicGrantsHero.tsx
@@ -33,9 +33,10 @@ export const AcademicGrantsHero: FC = () => {
           </PageSubheading>
 
           <PageText textAlign='center'>
-            The Ethereum Foundation is sponsoring a wave of grants to support academic work related
-            to Ethereum. This grants round has up to $750,000 in total available. Proposals are due{' '}
-            <strong>April 22, 2022</strong>. Here are all the details you need.
+            The Ethereum Foundation is sponsoring a wave of grants to support Ethereum-related
+            academic work. This grants round has up to $750,000 in total available. Proposals are
+            due <strong>April 22, 2022</strong>. All of the details you’ll need to apply can be
+            found below.
           </PageText>
         </Stack>
       </Center>
