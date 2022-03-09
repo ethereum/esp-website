54022315899e1b442f2a05e3cc4fdc8d7aabfaf7
Sam Richards <sbrichards@gmail.com>
2022-03-08 19:03:30 -0800

    Add additional meta tags

--- src/components/UI/PageMetadata.tsx
+++ src/components/UI/PageMetadata.tsx
@@ -15,6 +15,18 @@ export const PageMetadata: FC<Props> = ({ title, description }) => {
         {title} | {HEAD_TITLE}
       </title>
       <meta name='description' content={description} />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      <meta property='og:type' content='website' />
+      <meta property='og:url' content='https://esp.ethereum.foundation/' />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:creator' content='@EF_ESP' />
+      <meta name='twitter:site' content='@EF_ESP' />
+      <meta name='twitter:title' content='EF Ecosystem Support Program' />
+      <meta name='twitter:description' content={description} />
+      {/* TODO get absolute URL image */}
+      <meta name='twitter:image' content='GET_IMAGE_URL' />
+      <meta property='og:image' content='GET_IMAGE_URL' />
     </Head>
   );
 };
