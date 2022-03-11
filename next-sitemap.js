04b60fb4af2d7116f2338d570d6320be67555958
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-03-11 18:16:40 -0300

    feat: add next-sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,14 @@
+import { SITE_URL } from './src/constants';
+
+module.exports = {
+  siteUrl: SITE_URL,
+  exclude: [
+    '/applicants/grantee-finance/thank-you',
+    '/applicants/office-hours/thank-you',
+    '/applicants/project-grants/thank-you',
+    '/applicants/small-grants/thank-you',
+    '/academic-grants/thank-you',
+    '/devcon-grants/thank-you',
+    '/applicants/grantee-finance'
+  ]
+};
