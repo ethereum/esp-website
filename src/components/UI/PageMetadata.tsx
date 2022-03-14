8cfd37604c6b039beddda63aaa102fd16d1cc35f
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-03-14 18:53:08 -0300

    chore: update metadata

--- src/components/UI/PageMetadata.tsx
+++ src/components/UI/PageMetadata.tsx
@@ -16,10 +16,32 @@ export const PageMetadata: FC<Props> = ({ title, description }) => {
       </title>
       <meta name='title' content={`${title} | ${HEAD_TITLE}`} />
       <meta name='description' content={description} />
+      <meta name='application-name' content='Ethereum Foundation ESP' />
+      <meta
+        name='image'
+        content='https://esp.ethereum.foundation/images/homepage-hero-mobile.png'
+      />
+      {/* OpenGraph */}
       <meta property='og:title' content={`${title} | ${HEAD_TITLE}`} />
       <meta property='og:description' content={description} />
       <meta property='og:type' content='website' />
+      <meta property='og:site_name' content='Ethereum Foundation ESP'></meta>
       <meta property='og:url' content='https://esp.ethereum.foundation/' />
+      <meta
+        property='og:image'
+        content='https://esp.ethereum.foundation/images/homepage-hero-mobile.png'
+      />
+      <meta
+        property='og:image:url'
+        content='https://esp.ethereum.foundation/images/homepage-hero-mobile.png'
+      />
+      <meta
+        property='og:image:secure_url'
+        content='https://esp.ethereum.foundation/images/homepage-hero-mobile.png'
+      />
+      <meta property='og:image:alt' content='EF Ecosystem Support Program' />
+      <meta property='og:image:type' content='image/png' />
+      {/* Twitter */}
       <meta name='twitter:card' content='summary_large_image' />
       <meta property='twitter:url' content='https://esp.ethereum.foundation/' />
       <meta name='twitter:creator' content='@EF_ESP' />
@@ -28,10 +50,6 @@ export const PageMetadata: FC<Props> = ({ title, description }) => {
       <meta name='twitter:description' content={description} />
       <meta
         name='twitter:image'
-        content='https://esp.ethereum.foundation/images/homepage-hero-mobile.png'
-      />
-      <meta
-        property='og:image'
         content='https://esp.ethereum.foundation/images/homepage-hero-mobile.png'
       />
     </Head>
