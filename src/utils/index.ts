73aa63292467930eb73b9ccde257c498fb233349
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-03-14 15:34:16 -0300

    chore: remove unused getGithub util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
 export * from './getBg';
 export * from './getBgGradient';
-export * from './getGitHub';
 export * from './getLayoutHeight';
 export * from './getWebsite';
 export * from './selectedLink';
