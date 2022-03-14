67eb6cb433dd5b317f33c07ae8c38f2b428f6e55
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-03-14 17:16:50 -0300

    chore: update PageMetadata with image URL

--- src/components/UI/PageMetadata.tsx
+++ src/components/UI/PageMetadata.tsx
@@ -14,19 +14,26 @@ export const PageMetadata: FC<Props> = ({ title, description }) => {
       <title>
         {title} | {HEAD_TITLE}
       </title>
+      <meta name='title' content={title} />
       <meta name='description' content={description} />
       <meta property='og:title' content={title} />
       <meta property='og:description' content={description} />
       <meta property='og:type' content='website' />
       <meta property='og:url' content='https://esp.ethereum.foundation/' />
       <meta name='twitter:card' content='summary_large_image' />
+      <meta property='twitter:url' content='https://esp.ethereum.foundation/' />
       <meta name='twitter:creator' content='@EF_ESP' />
       <meta name='twitter:site' content='@EF_ESP' />
       <meta name='twitter:title' content='EF Ecosystem Support Program' />
       <meta name='twitter:description' content={description} />
-      {/* TODO get absolute URL image */}
-      <meta name='twitter:image' content='GET_IMAGE_URL' />
-      <meta property='og:image' content='GET_IMAGE_URL' />
+      <meta
+        name='twitter:image'
+        content='https://esp.ethereum.foundation/images/homepage-hero-mobile.png'
+      />
+      <meta
+        property='og:image'
+        content='https://esp.ethereum.foundation/images/homepage-hero-mobile.png'
+      />
     </Head>
   );
 };
