95a922bea9389e29b7887bf31233190e76173174
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-03-17 17:23:05 -0300

    chore: update constants

--- src/components/forms/constants.ts
+++ src/components/forms/constants.ts
@@ -1488,3 +1488,4 @@ export const API_SMALL_GRANTS_EVENT = '/api/small-grants/event';
 export const API_ACADEMIC_GRANTS = '/api/academic-grants';
 export const API_DEVCON_GRANTS = '/api/devcon-grants';
 export const API_GRANTEE_FINANCE = '/api/grantee-finance';
+export const API_NEWSLETTER_SIGNUP_URL = '/api/newsletter-signup';
