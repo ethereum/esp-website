21ddd45b17bd698a9cbd2ea39a7356193e53adc0
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-05-09 10:22:18 -0300

    chore: remove Academic Grants Round banner

--- src/components/Banners.tsx
+++ src/components/Banners.tsx
@@ -11,18 +11,20 @@ interface Props {}
 export const Banners: FC<Props> = () => {
   const router = useRouter();
 
-  if (!router.pathname.includes(ACADEMIC_GRANTS_URL)) {
-    return (
-      <BannerClickeable to={ACADEMIC_GRANTS_URL}>
-        <Box fontSize='paragraph' textAlign='center'>
-          We have an open call application for Academic Grants Round.{' '}
-          <Text as='u' fontWeight={700}>
-            See the details and Apply.
-          </Text>
-        </Box>
-      </BannerClickeable>
-    );
-  }
+  // Academic Grants Round is currently closed
+
+  // if (!router.pathname.includes(ACADEMIC_GRANTS_URL)) {
+  //   return (
+  //     <BannerClickeable to={ACADEMIC_GRANTS_URL}>
+  //       <Box fontSize='paragraph' textAlign='center'>
+  //         We have an open call application for Academic Grants Round.{' '}
+  //         <Text as='u' fontWeight={700}>
+  //           See the details and Apply.
+  //         </Text>
+  //       </Box>
+  //     </BannerClickeable>
+  //   );
+  // }
 
   return null;
 };
