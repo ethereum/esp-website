3979dbe5861b45adb97ca70e329b5221d180ff14
Corwin Smith <cssmittys@gmail.com>
2022-08-01 12:16:16 -0600

    Update banner.tsx for merge data challenge

--- src/components/Banners.tsx
+++ src/components/Banners.tsx
@@ -4,27 +4,25 @@ import { FC } from 'react';
 
 import { BannerClickeable } from './UI';
 
-import { ACADEMIC_GRANTS_URL } from '../constants';
+import { MERGE_DATA_CHALLENGE_URL } from '../constants';
 
 interface Props {}
 
 export const Banners: FC<Props> = () => {
   const router = useRouter();
 
-  // Academic Grants Round is currently closed
-
-  // if (!router.pathname.includes(ACADEMIC_GRANTS_URL)) {
-  //   return (
-  //     <BannerClickeable to={ACADEMIC_GRANTS_URL}>
-  //       <Box fontSize='paragraph' textAlign='center'>
-  //         We have an open call application for Academic Grants Round.{' '}
-  //         <Text as='u' fontWeight={700}>
-  //           See the details and Apply.
-  //         </Text>
-  //       </Box>
-  //     </BannerClickeable>
-  //   );
-  // }
+  if (!router.pathname.includes(MERGE_DATA_CHALLENGE_URL)) {
+    return (
+      <BannerClickeable to={MERGE_DATA_CHALLENGE_URL}>
+        <Box fontSize='paragraph' textAlign='center'>
+          We have an open call application for the Merge data challenge.{' '}
+          <Text as='u' fontWeight={700}>
+            See the details and Apply.
+          </Text>
+        </Box>
+      </BannerClickeable>
+    );
+  }
 
   return null;
 };
