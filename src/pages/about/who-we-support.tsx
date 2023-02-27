70aeb91083015252be230ce2351a6dc0b0f63a1d
Pablo Pettinari <pettinarip@gmail.com>
2023-02-27 11:48:33 -0300

    revert test code

--- src/pages/about/who-we-support.tsx
+++ src/pages/about/who-we-support.tsx
@@ -24,7 +24,7 @@ export const getStaticProps: GetStaticProps = async context => {
         });
       });
     })
-    .catch(err => console.error(err?.toJSON?.() ?? err));
+    .catch(err => console.error(err.toJSON()));
 
   return {
     props: {
