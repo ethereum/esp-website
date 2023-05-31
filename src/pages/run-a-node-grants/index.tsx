02381c0023acfc224cac20ccdf2f1fa66a08dc0c
Pablo Pettinari <pettinarip@gmail.com>
2023-05-31 16:55:24 -0300

    add metadata

--- src/pages/run-a-node-grants/index.tsx
+++ src/pages/run-a-node-grants/index.tsx
@@ -40,7 +40,7 @@ const RunANodeGrants: NextPage = () => {
     <>
       <PageMetadata
         title='Run A Node Grants'
-        description='' // TODO
+        description='The Ethereum Foundation is calling for proposals to expand the diversity of nodes within its network'
       />
 
       <Box bg='white' position='relative' mt={{ xl: 12 }}>
