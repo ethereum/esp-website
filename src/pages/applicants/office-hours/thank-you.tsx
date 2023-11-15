b3c2b93037de8fd3bb2d05d74ea025f55c810c53
Pablo Pettinari <pettinarip@gmail.com>
2023-11-15 17:58:39 +0300

    change the thankyou page body copy

--- src/pages/applicants/office-hours/thank-you.tsx
+++ src/pages/applicants/office-hours/thank-you.tsx
@@ -1,8 +1,8 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Heading, Link, Stack } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-import { PageMetadata, ThankYouBody } from '../../../components/UI';
+import { PageMetadata, PageSubheading, PageText } from '../../../components/UI';
 
 const OfficeHoursThankYou: NextPage = () => {
   return (
@@ -13,7 +13,37 @@ const OfficeHoursThankYou: NextPage = () => {
       </Head>
 
       <Box bg='white' position='relative' py={{ md: 12 }} px={{ md: 20, lg: 24, xl: 72 }}>
-        <ThankYouBody grantType='Office Hours' />
+        <Stack mb={10}>
+          <section id='description'>
+            <Heading
+              as='h1'
+              color='brand.heading'
+              fontSize='h1'
+              fontWeight={100}
+              lineHeight='48px'
+              textAlign='center'
+              mb={4}
+            >
+              Thank you!
+            </Heading>
+
+            <PageSubheading mb={16} textAlign='center'>
+              for applying to Office Hours
+            </PageSubheading>
+
+            <PageText mb={6} textAlign='center'>
+              You should receive a confirmation email shortly. For more information, refer to the{' '}
+              <Link
+                fontWeight={700}
+                color='brand.orange.100'
+                href='/applicants/office-hours'
+                _hover={{ textDecoration: 'none' }}
+              >
+                Office Hours About page.
+              </Link>
+            </PageText>
+          </section>
+        </Stack>
       </Box>
     </>
   );
