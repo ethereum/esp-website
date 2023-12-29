85da1cc56cd88cf5fe6fe88423bd180af7c62d9b
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-12-28 21:22:57 -0300

    chore: add PSE_GRANTS urls

--- src/components/forms/constants.ts
+++ src/components/forms/constants.ts
@@ -1548,5 +1548,6 @@ export const API_ACADEMIC_GRANTS = '/api/academic-grants';
 export const API_DEVCON_GRANTS = '/api/devcon-grants';
 export const API_GRANTEE_FINANCE = '/api/grantee-finance';
 export const API_ECODEV_GRANTS = '/api/ecodev-grants';
+export const API_PSE_GRANTS = '/api/pse-grants';
 export const API_DATA_COLLECTION_GRANTS = '/api/data-collection-grants';
 export const API_NEWSLETTER_SIGNUP_URL = '/api/newsletter-signup';
