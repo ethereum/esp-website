8dd0ab2105098e8c6532f8cac8d5cfcde07f5a51
Pablo Pettinari <pettinarip@gmail.com>
2024-01-19 11:48:25 +0100

    add banner

--- src/components/Banners.tsx
+++ src/components/Banners.tsx
@@ -1,5 +1,27 @@
 import { FC } from 'react';
+import { useRouter } from 'next/router';
+import { Box, Link } from '@chakra-ui/react';
+
+import { Banner } from './UI';
+
+import { ZK_GRANTS_URL } from '../constants';
 
 export const Banners: FC = () => {
+  const router = useRouter();
+
+  if (!router.pathname.includes(ZK_GRANTS_URL)) {
+    return (
+      <Banner>
+        <Box fontSize='paragraph' textAlign='center'>
+          Applications are open for the{' '}
+          <Link fontWeight={700} href={ZK_GRANTS_URL}>
+            ZK Grants Round
+          </Link>{' '}
+          . See the details and apply.
+        </Box>
+      </Banner>
+    );
+  }
+
   return null;
 };
