72a32ff3a9219e2c3a03313e0d2a8a4ecf3c003d
Pablo Pettinari <pettinarip@gmail.com>
2024-02-05 16:40:24 +0100

    fix conditional field condition in schema

--- src/components/forms/schemas/AcademicGrants.ts
+++ src/components/forms/schemas/AcademicGrants.ts
@@ -10,56 +10,65 @@ const MIN_TEXT_AREA_LENGTH = 500;
 
 const ACCEPTED_FILE_TYPES = ['application/pdf'];
 
-export const AcademicGrantsSchema = z.object({
-  firstName: stringFieldSchema('First name', { min: 1, max: 40 }).refine(
-    value => !containURL(value),
-    'First name cannot contain a URL'
-  ),
-  lastName: stringFieldSchema('Last name', { min: 1, max: 80 }).refine(
-    value => !containURL(value),
-    'Last name cannot contain a URL'
-  ),
-  email: z.string().email({ message: 'Invalid email address' }),
-  POCisAuthorisedSignatory: z.boolean(),
-  authorisedSignatoryInformation: stringFieldSchema('Authorised signatory information', {
-    min: 1,
-    max: MAX_TEXT_LENGTH
-  }).optional(),
-  applyingAs: z.string().min(1, 'Please select in which capacity you are applying.'),
-  applyingAsOther: stringFieldSchema('Field', { max: MAX_TEXT_LENGTH }).optional(),
-  company: stringFieldSchema('Organization', { min: 1, max: MAX_TEXT_LENGTH }).refine(
-    value => !containURL(value),
-    'Organization cannot contain a URL'
-  ),
-  country: stringFieldSchema('Country', { min: 1 }),
-  countriesTeam: stringFieldSchema('Countries of team', { min: 1 }),
-  timezone: stringFieldSchema('Time zone', { min: 1 }),
-  projectName: stringFieldSchema('Project name', { min: 1, max: MAX_TEXT_LENGTH }),
-  projectDescription: stringFieldSchema('Project description', {
-    min: MIN_TEXT_AREA_LENGTH,
-    max: MAX_TEXT_AREA_LENGTH
-  }),
-  projectCategory: stringFieldSchema('Project category', { min: 1 }),
-  requestAmount: stringFieldSchema('Total budget', { max: 20 }).optional(),
-  referralSource: stringFieldSchema('Referral source', { min: 1 }),
-  referralSourceIfOther: stringFieldSchema('Field', { max: MAX_TEXT_AREA_LENGTH }).optional(),
-  proposalAttachment: z
-    .any()
-    .refine(file => !!file, 'Proposal is required.')
-    .refine(file => file?.size <= MAX_PROPOSAL_FILE_SIZE, `Max file size is 4MB.`)
-    .refine(
-      file => ACCEPTED_FILE_TYPES.includes(file?.type || file?.mimetype),
-      'Only .pdf files are accepted.'
+export const AcademicGrantsSchema = z
+  .object({
+    firstName: stringFieldSchema('First name', { min: 1, max: 40 }).refine(
+      value => !containURL(value),
+      'First name cannot contain a URL'
     ),
-  shareResearch: stringFieldSchema('Share research', { min: 1 }),
-  website: stringFieldSchema('Website', { max: MAX_TEXT_LENGTH }).optional(),
-  linkedinProfile: stringFieldSchema('LinkedIn profiles', { max: MAX_TEXT_LENGTH }).optional(),
-  twitter: stringFieldSchema('Twitter handle', { max: 16 }).optional(),
-  alternativeContact: stringFieldSchema('Alternative contact info', { max: 150 }).optional(),
-  repeatApplicant: z.boolean(),
-  canTheEFReachOut: z.boolean().optional(),
-  additionalInfo: stringFieldSchema('Additional info', { max: MAX_TEXT_AREA_LENGTH }).optional(),
-  captchaToken: stringFieldSchema('Captcha', { min: 1 })
-});
+    lastName: stringFieldSchema('Last name', { min: 1, max: 80 }).refine(
+      value => !containURL(value),
+      'Last name cannot contain a URL'
+    ),
+    email: z.string().email({ message: 'Invalid email address' }),
+    POCisAuthorisedSignatory: z.boolean(),
+    authorisedSignatoryInformation: stringFieldSchema('Authorised signatory information', {
+      max: MAX_TEXT_LENGTH
+    }).optional(),
+    applyingAs: z.string().min(1, 'Please select in which capacity you are applying.'),
+    applyingAsOther: stringFieldSchema('Field', { max: MAX_TEXT_LENGTH }).optional(),
+    company: stringFieldSchema('Organization', { min: 1, max: MAX_TEXT_LENGTH }).refine(
+      value => !containURL(value),
+      'Organization cannot contain a URL'
+    ),
+    country: stringFieldSchema('Country', { min: 1 }),
+    countriesTeam: stringFieldSchema('Countries of team', { min: 1 }),
+    timezone: stringFieldSchema('Time zone', { min: 1 }),
+    projectName: stringFieldSchema('Project name', { min: 1, max: MAX_TEXT_LENGTH }),
+    projectDescription: stringFieldSchema('Project description', {
+      min: MIN_TEXT_AREA_LENGTH,
+      max: MAX_TEXT_AREA_LENGTH
+    }),
+    projectCategory: stringFieldSchema('Project category', { min: 1 }),
+    requestAmount: stringFieldSchema('Total budget', { max: 20 }).optional(),
+    referralSource: stringFieldSchema('Referral source', { min: 1 }),
+    referralSourceIfOther: stringFieldSchema('Field', { max: MAX_TEXT_AREA_LENGTH }).optional(),
+    proposalAttachment: z
+      .any()
+      .refine(file => !!file, 'Proposal is required.')
+      .refine(file => file?.size <= MAX_PROPOSAL_FILE_SIZE, `Max file size is 4MB.`)
+      .refine(
+        file => ACCEPTED_FILE_TYPES.includes(file?.type || file?.mimetype),
+        'Only .pdf files are accepted.'
+      ),
+    shareResearch: stringFieldSchema('Share research', { min: 1 }),
+    website: stringFieldSchema('Website', { max: MAX_TEXT_LENGTH }).optional(),
+    linkedinProfile: stringFieldSchema('LinkedIn profiles', { max: MAX_TEXT_LENGTH }).optional(),
+    twitter: stringFieldSchema('Twitter handle', { max: 16 }).optional(),
+    alternativeContact: stringFieldSchema('Alternative contact info', { max: 150 }).optional(),
+    repeatApplicant: z.boolean(),
+    canTheEFReachOut: z.boolean().optional(),
+    additionalInfo: stringFieldSchema('Additional info', { max: MAX_TEXT_AREA_LENGTH }).optional(),
+    captchaToken: stringFieldSchema('Captcha', { min: 1 })
+  })
+  .refine(
+    data =>
+      data.POCisAuthorisedSignatory ||
+      (!data.POCisAuthorisedSignatory && data.authorisedSignatoryInformation),
+    {
+      message: `Authorised signatory information is required`,
+      path: ['authorisedSignatoryInformation']
+    }
+  );
 
 export type AcademicGrantsData = z.infer<typeof AcademicGrantsSchema>;
