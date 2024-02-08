f7378a1dd54bebf8acc05fdf5e89196964c39214
Pablo Pettinari <pettinarip@gmail.com>
2024-02-08 11:17:06 +0100

    fix banners display condition

--- src/components/Banners.tsx
+++ src/components/Banners.tsx
@@ -9,19 +9,19 @@ import { ACADEMIC_GRANTS_URL, ZK_GRANTS_URL } from '../constants';
 export const Banners: FC = () => {
   const router = useRouter();
 
-  if (!router.pathname.includes(ACADEMIC_GRANTS_URL) || !router.pathname.includes(ZK_GRANTS_URL)) {
+  if (!router.pathname.includes(ACADEMIC_GRANTS_URL) && !router.pathname.includes(ZK_GRANTS_URL)) {
     return (
       <Banner>
         <Box fontSize='paragraph' textAlign='center'>
           Applications are open for the{' '}
           <Link fontWeight={700} href={ACADEMIC_GRANTS_URL}>
-            Academic Grants Round
+            Academic Grants
           </Link>{' '}
           and{' '}
           <Link fontWeight={700} href={ZK_GRANTS_URL}>
-            ZK Grants Round
+            ZK Grants
           </Link>{' '}
-          . See the details and apply.
+          rounds. See the details and apply.
         </Box>
       </Banner>
     );
