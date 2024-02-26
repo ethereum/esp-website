391623043bb64829b4922c48ca76d3a6f0fe1401
Pablo Pettinari <pettinarip@gmail.com>
2024-02-26 10:43:08 +0100

    add new hero image

--- src/components/layout/DataChallengeLayout.tsx
+++ src/components/layout/DataChallengeLayout.tsx
@@ -3,22 +3,28 @@ import { FC } from 'react';
 
 import { GrantsHero } from '../UI';
 
-import academicGrantsHero from '../../../public/images/academic-grants-hero.png';
-import academicGrantsHeroMobile from '../../../public/images/academic-grants-hero-mobile.png';
+import dataChallengeGrantsHero from '../../../public/images/data-challenge-4844-hero.jpg';
 
 export const DataChallengeLayout: FC = ({ children }) => {
+  const alt =
+    'A person standing at the top of a hill looking at a vast landscape that contains things with the shape of different types of charts';
+
   return (
     <Stack>
       <GrantsHero
-        colorBrandConstant='academicGrantsHero'
-        desktopImage={{ alt: 'Academics doing research about Ethereum', src: academicGrantsHero }}
+        colorBrandConstant='dataChallengeHero'
+        desktopImage={{
+          alt,
+          src: dataChallengeGrantsHero
+        }}
         mobileImage={{
-          alt: 'Academics doing research about Ethereum',
-          src: academicGrantsHeroMobile
+          alt,
+          src: dataChallengeGrantsHero
         }}
         title='Data Challenge 4844'
       >
-        The Ethereum Foundation is sponsoring a 4844 data analysis and visualization blog post challenge. Here are all the details you need.
+        The Ethereum Foundation is sponsoring a 4844 data analysis and visualization blog post
+        challenge. Here are all the details you need.
       </GrantsHero>
 
       <Stack>{children}</Stack>
