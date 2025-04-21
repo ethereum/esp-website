0853cc6c07af9e4592e10c4c27ae143e0c0230a7
Corwin Smith <cssmittys@gmail.com>
2025-04-21 13:33:30 -0600

    banner

--- src/components/Banners.tsx
+++ src/components/Banners.tsx
@@ -1,18 +1,28 @@
 import { FC } from 'react';
 import { useRouter } from 'next/router';
+import { Box, Link } from '@chakra-ui/react';
+
+import { Banner } from './UI';
+
+import { ACADEMIC_GRANTS_URL, TEN_YEAR_ANNIVERSARY_URL } from '../constants';
 
 export const Banners: FC = () => {
   const router = useRouter();
 
   // TODO: Add banner content
-  // if (!router.pathname.includes(ACADEMIC_GRANTS_URL)) {
-  //   return (
-  //     <Banner>
-  //       <Box fontSize='paragraph' textAlign='center'>
-  //       </Box>
-  //     </Banner>
-  //   );
-  // }
+  if (!router.pathname.includes(ACADEMIC_GRANTS_URL)) {
+    return (
+      <Banner>
+        <Box fontSize='paragraph' textAlign='center'>
+          Applications for the{' '}
+          <Link href={TEN_YEAR_ANNIVERSARY_URL} fontWeight={700}>
+            10 Years of Ethereum Anniversary
+          </Link>{' '}
+          are now open!
+        </Box>
+      </Banner>
+    );
+  }
 
   return null;
 };
