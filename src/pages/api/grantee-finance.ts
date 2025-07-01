61976e0b3a7cb620d0daff18d1b2f91971b98b99
Pablo <pettinarip@gmail.com>
2025-07-01 15:25:03 +0200

    remove security id field from fields to update

--- src/pages/api/grantee-finance.ts
+++ src/pages/api/grantee-finance.ts
@@ -64,7 +64,6 @@ async function handler(req: GranteeFinanceNextApiRequest, res: NextApiResponse):
               // SF expects an `Id` field, with the id of the object you want to update as value
               // We're updating the Contract object in this case, so `Id` should be `Contract_ID__c`
               Contract_ID__c: Contract_ID__c.trim(),
-              Security_ID__c: Security_ID__c.trim(),
               Beneficiary_Name__c: Beneficiary_Name__c.trim(),
               User_Email__c: User_Email__c.trim(),
               Transfer_Notes__c: Transfer_Notes__c.trim(),
