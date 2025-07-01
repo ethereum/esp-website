861ed5dd90d7837dca3884404c38a47961d4f9ea
Pablo <pettinarip@gmail.com>
2025-07-01 15:37:50 +0200

    use id

--- src/pages/api/grantee-finance.ts
+++ src/pages/api/grantee-finance.ts
@@ -63,7 +63,7 @@ async function handler(req: GranteeFinanceNextApiRequest, res: NextApiResponse):
             {
               // SF expects an `Id` field, with the id of the object you want to update as value
               // We're updating the Contract object in this case, so `Id` should be `Contract_ID__c`
-              Contract_ID__c: Contract_ID__c.trim(),
+              Id: Contract_ID__c.trim(),
               Beneficiary_Name__c: Beneficiary_Name__c.trim(),
               User_Email__c: User_Email__c.trim(),
               Transfer_Notes__c: Transfer_Notes__c.trim(),
