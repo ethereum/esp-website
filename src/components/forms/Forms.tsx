ef1c67f6636924fb6331f73008902a32ef537580
Pablo Pettinari <pettinarip@gmail.com>
2025-12-17 16:27:52 +0100

    Add GranteeFinanceForm to Forms component and export from index

--- src/components/forms/Forms.tsx
+++ src/components/forms/Forms.tsx
@@ -2,15 +2,20 @@ import { Box, Stack } from '@chakra-ui/react';
 import { FC } from 'react';
 import { useRouter } from 'next/router';
 
-import { OfficeHoursForm } from './';
+import { GranteeFinanceForm, OfficeHoursForm } from './';
 
-import { OFFICE_HOURS_APPLY_URL } from '../../constants';
+import { GRANTEE_FINANCE_URL, OFFICE_HOURS_APPLY_URL } from '../../constants';
 
 export const Forms: FC = () => {
   const router = useRouter();
 
   return (
     <Stack id='forms'>
+      {router.pathname === GRANTEE_FINANCE_URL && (
+        <Box px={{ md: 24, lg: 32, xl: 72 }}>
+          <GranteeFinanceForm />
+        </Box>
+      )}
       {router.pathname === OFFICE_HOURS_APPLY_URL && (
         <Box px={{ md: 24, lg: 32, xl: 72 }}>
           <OfficeHoursForm />
