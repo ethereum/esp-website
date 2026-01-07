f80cc5e17dd1e43c652ee969f2a6f4e1f268c416
Pablo Pettinari <pettinarip@gmail.com>
2026-01-07 17:36:59 +0100

    Remove minimum character limits for Direct Grant form fields (issue #490)

--- src/components/forms/schemas/DirectGrant.ts
+++ src/components/forms/schemas/DirectGrant.ts
@@ -6,11 +6,7 @@ import {
   requiredSchema
 } from './BaseGrant';
 import { z } from 'zod';
-import {
-  MAX_TEXT_AREA_LENGTH,
-  MIN_TEXT_AREA_LENGTH,
-  MAX_WISHLIST_FILE_SIZE
-} from '../../../constants';
+import { MAX_TEXT_AREA_LENGTH, MAX_WISHLIST_FILE_SIZE } from '../../../constants';
 import { stringFieldSchema } from './utils';
 
 export const DirectGrantSchema = z.object({
@@ -19,11 +15,17 @@ export const DirectGrantSchema = z.object({
   ...projectDetailsSchema,
   ...additionalDetailsSchema,
   ...requiredSchema,
-  applicantProfile: stringFieldSchema('Applicant profile', {
-    min: MIN_TEXT_AREA_LENGTH,
-    max: MAX_TEXT_AREA_LENGTH
-  }),
-  // File upload field
+
+  projectSummary: stringFieldSchema('Project summary', { min: 250, max: MAX_TEXT_AREA_LENGTH }),
+  applicantProfile: stringFieldSchema('Applicant profile', { min: 1, max: MAX_TEXT_AREA_LENGTH }),
+  projectStructure: stringFieldSchema('Project structure', { min: 1, max: MAX_TEXT_AREA_LENGTH }),
+  sustainabilityPlan: stringFieldSchema('Sustainability plan', { min: 1, max: MAX_TEXT_AREA_LENGTH }),
+  funding: stringFieldSchema('Other funding', { min: 1, max: MAX_TEXT_AREA_LENGTH }),
+  problemBeingSolved: stringFieldSchema('Problem being solved', { min: 1, max: MAX_TEXT_AREA_LENGTH }),
+  measuredImpact: stringFieldSchema('Measured impact', { min: 1, max: MAX_TEXT_AREA_LENGTH }),
+  successMetrics: stringFieldSchema('Success metrics', { min: 1, max: MAX_TEXT_AREA_LENGTH }),
+  ecosystemFit: stringFieldSchema('Ecosystem fit', { min: 1, max: MAX_TEXT_AREA_LENGTH }),
+  communityFeedback: stringFieldSchema('Community feedback', { min: 1, max: MAX_TEXT_AREA_LENGTH }),
   fileUpload: z
     .any()
     .optional()
