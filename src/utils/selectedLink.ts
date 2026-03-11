0bad14c72ac114023170927f8697d3e13cd6cb5e
Pablo Pettinari <pettinarip@gmail.com>
2026-03-11 08:52:11 +0100

    fix(nav): highlight Funded Projects link when on /grants pages

--- src/utils/selectedLink.ts
+++ src/utils/selectedLink.ts
@@ -1,6 +1,7 @@
-import { ABOUT_URL, APPLICANTS_URL, HOME_URL } from '../constants';
+import { ABOUT_URL, APPLICANTS_URL, GRANTS_URL, HOME_URL } from '../constants';
 
 export const selectedLink = (pathname: string, href: string) =>
   (pathname === HOME_URL && href === HOME_URL) ||
+  (pathname.startsWith(GRANTS_URL) && href === GRANTS_URL) ||
   (pathname.startsWith(APPLICANTS_URL) && href === APPLICANTS_URL) ||
   (pathname.startsWith(ABOUT_URL) && href === ABOUT_URL);
